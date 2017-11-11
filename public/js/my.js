3f4c2141dac164a0dceb7dab56f4e5ebb56fa865
email <harazdovskiy@gmail.com>
2017-11-11 13:45:13 +0200

    Add login and regist show and hide

--- public/js/my.js
+++ public/js/my.js
@@ -1,58 +1,16 @@
 //Під`єднюємо ангуляр
 const app = angular.module('app', ['ngRoute', 'ngDialog']);
 
+
+
 //Створюємо контроллер
-app.controller('myCtrl', function ($scope, $http, ngDialog) {});
-
-
-
-//Директива Login
-app.directive('loginBlock', function () {
-    return {
-        replace: true,
-        templateUrl: 'template/login.html',
-        controller: function ($scope, ngDialog) {
-            $scope.loginStatus = true;
-            $scope.registrStatus = false;
-            //Кнопка Реєстрації
-            $scope.chooseRegistr = function () {
-                $scope.loginStatus = false;
-                $scope.contentStatus = false;
-                $scope.registrStatus = true;
-                $scope.headerStatus = false;
-                $scope.menuStatus = false;
-            }
-
-            //при натискані кнопки Registration ngDialog
-            $scope.registerDialWind = function () {
-                ngDialog.open({
-                    template: "/template/registr.html",
-                    scope: $scope,
-                    controller: function () {
-                        $scope.loginStatus = false;
-                        $scope.registerStatus = true;
-
-                        $scope.registerAcc = function () {
-                            $scope.loginStatus = true;
-                            $scope.registerStatus = false;
-                        }
-
-                    }
-                })
-            }
-
-
-        }
-    }
-});
-
-//Директива Registration
-app.directive('registrBlock', function () {
-    return {
-        replace: true,
-        templateUrl: 'template/registr.html',
-        controller: function ($scope) {}
-    }
+app.controller('myCtrl', function ($scope, $http, ngDialog) {
+    
+    $scope.menuBlock = false;
+    $scope.headerBlock = false;
+    $scope.loginBlock = true;
+    
+    
 });
 
 //Директива header
@@ -65,34 +23,19 @@ app.directive('headerBlock', function () {
             $scope.contentStatus = false;
             $scope.registrStatus = true;
             //Кнопка SIGN UP
-            $scope.chooseSignUp = function () {
+           /* $scope.chooseSignUp = function () {
                 $scope.loginStatus = true;
                 $scope.contentStatus = false;
                 $scope.registrStatus = false;
                 $scope.headerStatus = false;
                 $scope.menuStatus = false;
 
-            }
+            }*/
         }
     }
 });
 
-//Директива Content
-app.directive('contentBlock', function () {
-    return {
-        replace: true,
-        templateUrl: 'template/content.html',
-        controller: function ($scope) {
-            $scope.loginStatus = false;
-            $scope.registrStatus = false;
-            $scope.contentStatus = true;
-            $scope.headerStatus = true;
-            $scope.menuStatus = true;
-        }
-    }
-});
-
-
+/*навігація*/
 app.directive('navBlock', function () {
     return {
         replace: true,
@@ -103,3 +46,43 @@ app.directive('navBlock', function () {
         }
     }
 });
+
+//Директива Content
+app.directive('contentBlock', function () {
+    return {
+        replace: true,
+        templateUrl: 'template/content.html',
+        controller: function ($scope) {
+
+        }
+    }
+});
+
+//Директива Login
+app.directive('loginBlock', function () {
+    return {
+        replace: true,
+        templateUrl: 'template/pages/login.html',
+        controller: function ($scope, ngDialog) {
+            $scope.Registr = function(){
+                
+                $scope.registerBlock = true;
+                $scope.loginBlock = false;
+            }
+        }
+    }
+});
+
+//Директива Registration
+app.directive('registrBlock', function () {
+    return {
+        replace: true,
+        templateUrl: 'template/pages/registr.html',
+        controller: function ($scope) {
+            
+        }
+    }
+});
+
+
+
