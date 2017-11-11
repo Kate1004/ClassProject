15c9eec3931c4d138d4fec5d87331ef6b532576e
Roman Fylypiuk <roman.if.ko@gmail.com>
2017-11-11 14:34:20 +0200

    добавив масив з даними для реєстрації
    добавив масив з даними для реєстрації

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,107 @@
-var users=[
+var users = [
     {
-        id:1,
-        login:'Yura',
-        password: 'some1234',
-        eMail: 'some@some.s',
-        name: 'Yura',
-        sname: 'Volchak',
-        bDay: '1999-05-25',
-        photo: '1.png',
-        sourses: [
-        {   id: 1,
-            name: 'freelance',
-            date: '2017-11-09'},
-        {   id: 2,
-            name: 'present',
-            date: '2017-11-08'},
-        {   id: 3,
-            name: 'credit',
-            date: '2017-11-09'},
-        ],
-        catExpenses:[
+        id: 1,
+        login: "Yura",
+        password: "some12345",
+        eMail: "some@some.s",
+        name: "Yura",
+        sname: "Volchak",
+        bDay: "1987-05-25",
+        photo: "1.png",
+        sources: [
             {
-                id:1,
-                name:'car',
-            },{
-                id:2,
-                name:'food',
-            },{
-                id:3,
-                name:'house',
+                id: 1,
+                sName: "freelance",
+                date: "2017-11-07"
+             }
+            , {
+                id: 24,
+                sName: "present",
+                date: "2017-11-08"
+             }
+            , {
+                id: 35,
+                sName: "credit",
+                date: "2017-11-09"
+             }
+
+        , ],
+        catExpenses: [
+            {
+                id: 1,
+                name: "car",
+            },
+            {
+                id: 2,
+                name: "food",
+            },
+            {
+                id: 7,
+                name: "house"
+            },
+        ],
+        saves: [
+            {
+                id: 3,
+                name: "visa",
+                sum: 23456,
+                date: "2017-11-07",
+
+            },
+            {
+                id: 4,
+                name: "cash",
+                sum: 456,
+                date: "2017-11-08",
+
+            },
+            {
+                id: 7,
+                name: "bank",
+                sum: 2346,
+                date: "2017-11-07",
+
             }
         ],
-        saves:[
+        incomes: [
             {
-                id:3,
-                name: 'visa',
-                sum: 234,
-                date: '2017-11-07'
-            },{
-                id:7,
-                name: 'bank',
-                sum: 2343,
-                date: '2017-11-07'
-            },{
-                id:8,
-                name: 'cesh',
-                sum: 23,
-                date: '2017-11-07'
+                id: 2,
+                date: "2017-11-09",
+                sum: 200,
+                sourceId: 1,
+                comments: " text"
+
+            },
+            {
+                id: 5,
+                date: "2017-11-09",
+                sum: 400,
+                sourceId: 1,
+                comments: "dgf"
+            },
+            {
+                id: 7,
+                date: "2017-11-09",
+                sum: 200,
+                sourceId: 35,
+                comments: " fhfh"
+            }
+        ],
+        expences: [
+            {
+                id: 2,
+                date: "2017-11-12",
+                sum: "299",
+                comments: "some text",
+                catExpensesId: 2
+            },
+            {
+                id: 3,
+                date: "2017-11-12",
+                sum: "100",
+                comments: "some text",
+                catExpensesId: 1
             }
         ]
     }
-    
 ]
-
-//XAXAAXAXAXAXAXAXAXAXAXAXAXAXAXAXAAXAAXAXAXAXAXAXAXAXAXAXAXAXAXA
-//Катя прівєт!
