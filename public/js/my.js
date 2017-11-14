a18e3c4fb29591dda60bd422c68d6e8e4dad4253
Petro <petro.petr@gmail.com>
2017-11-14 18:12:25 +0200

    created history html file, directive in js, fill it with buttons, tables and use ng-repeat to take from js object users

--- public/js/my.js
+++ public/js/my.js
@@ -1,7 +1,7 @@
 //Під`єднюємо ангуляр
 const app = angular.module('app', ['ngRoute', 'ngDialog']);
 // структура об'єкту
-//var users = [
+//$scope.users = [
 //    {
 //        id: 1,
 //        login: "Yura",
@@ -108,6 +108,14 @@ const app = angular.module('app', ['ngRoute', 'ngDialog']);
 //        ]
 //    }
 //];
+
+//$scope.historyObject = [{
+//    date: ""
+//}]
+
+//console.log(users[0].expences[0].date)
+
+
 //Створюємо контроллер
 app.controller('myCtrl', function ($scope, $http, ngDialog) {
     /*Включаємо форму логування*/
@@ -181,22 +189,32 @@ app.directive('navBlock', function () {
                 $scope.accountBlock = false;
                 $scope.homeBlock = false;
                 $scope.statsBlock = false;
+                $scope.historyBlock = false;
             }
 
             $scope.usersPage = function () {
                 $scope.accountBlock = true;
                 $scope.homeBlock = false;
                 $scope.statsBlock = false;
+                $scope.historyBlock = false;
             }
             $scope.statsPage = function () {
                 $scope.accountBlock = false;
                 $scope.homeBlock = false;
                 $scope.statsBlock = true;
+                $scope.historyBlock = false;
             }
-               $scope.homePage = function () {
+            $scope.homePage = function () {
                 $scope.accountBlock = false;
                 $scope.homeBlock = true;
                 $scope.statsBlock = false;
+                $scope.historyBlock = false;
+            }
+            $scope.historyPage = function () {
+                $scope.accountBlock = false;
+                $scope.homeBlock = false;
+                $scope.statsBlock = false;
+                $scope.historyBlock = true;
             }
 
         }
@@ -254,8 +272,7 @@ app.directive('homeBlock', function () {
     return {
         replace: true,
         templateUrl: 'template/pages/home.html',
-        controller: function ($scope) {
-        }
+        controller: function ($scope) {}
     }
 });
 
@@ -267,13 +284,132 @@ app.directive('accountBlock', function () {
         controller: function ($scope) {}
     }
 });
+
+
+//directive history
+app.directive('historyBlock', function () {
+    return {
+        replace: true,
+        templateUrl: 'template/pages/history.html',
+        controller: function ($scope) {
+            $scope.users = [
+                {
+                    id: 1,
+                    login: "Yura",
+                    password: "some12345",
+                    eMail: "some@some.s",
+                    name: "Yura",
+                    sname: "Volchak",
+                    bDay: "1987-05-25",
+                    photo: "1.png",
+                    sources: [
+                        {
+                            id: 1,
+                            sName: "freelance",
+                            date: "2017-11-07"
+             }
+            , {
+                            id: 24,
+                            sName: "present",
+                            date: "2017-11-08"
+             }
+            , {
+                            id: 35,
+                            sName: "credit",
+                            date: "2017-11-09"
+             }
+
+        , ],
+                    catExpenses: [
+                        {
+                            id: 1,
+                            name: "car",
+            },
+                        {
+                            id: 2,
+                            name: "food",
+            },
+                        {
+                            id: 7,
+                            name: "house"
+            },
+        ],
+                    saves: [
+                        {
+                            id: 3,
+                            name: "visa",
+                            sum: 23456,
+                            date: "2017-11-07",
+
+            },
+                        {
+                            id: 4,
+                            name: "cash",
+                            sum: 456,
+                            date: "2017-11-08",
+
+            },
+                        {
+                            id: 7,
+                            name: "bank",
+                            sum: 2346,
+                            date: "2017-11-07",
+
+            }
+        ],
+                    incomes: [
+                        {
+                            id: 2,
+                            date: "2017-11-04",
+                            sum: 200,
+                            sourceId: 1,
+                            comments: " text"
+
+            },
+                        {
+                            id: 5,
+                            date: "2017-11-09",
+                            sum: 400,
+                            sourceId: 1,
+                            comments: "dgf"
+            },
+                        {
+                            id: 7,
+                            date: "2017-11-09",
+                            sum: 200,
+                            sourceId: 35,
+                            comments: " fhfh"
+            }
+        ],
+                    expences: [
+                        {
+                            id: 2,
+                            date: "2017-11-12",
+                            sum: "299",
+                            comments: "some text",
+                            catExpensesId: 2
+            },
+                        {
+                            id: 3,
+                            date: "2017-11-15",
+                            sum: "100",
+                            comments: "some text",
+                            catExpensesId: 1
+            }
+        ]
+    }
+];
+        }
+    }
+});
+
 //директива статистика
 app.directive('statsBlock', function () {
     return {
         replace: true,
         templateUrl: 'template/pages/statistic.html',
         controller: function ($scope) {
-               $scope.day = false;
+            $scope.day = false;
             $scope.dates = [
                 "1",
                 "2",
@@ -328,18 +464,18 @@ app.directive('statsBlock', function () {
                 "2019",
                 "2020"
             ]
-            $scope.selectDay = function() {
+            $scope.selectDay = function () {
                 $scope.day = true;
                 $scope.month = false;
                 $scope.year = false;
-                
+
             }
-             $scope.selectMonth = function() {
+            $scope.selectMonth = function () {
                 $scope.day = false;
                 $scope.month = true;
                 $scope.year = false;
             }
-              $scope.SelectYear = function() {
+            $scope.SelectYear = function () {
                 $scope.day = false;
                 $scope.month = false;
                 $scope.year = true;
